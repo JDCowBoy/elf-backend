487b886931e630adeef4c4c2529101d6bb92c284
JDCowBoy <280128206@qq.com>
2020-02-23 16:04:35 +0800

    增加基础静态css,js等文件

---
+++ src/main/java/com/kaoho/elf/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.kaoho.elf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 首页
+ */
+@Controller
+public class IndexController extends BaseController
+{
+
+
+    //系统首页
+    @GetMapping(value = "/")
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index()
+    {
+        System.out.println("!!!!!!!!!!!!");
+        return "index";
+    }
+
+}

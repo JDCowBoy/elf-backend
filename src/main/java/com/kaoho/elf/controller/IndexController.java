4f24ba90c8c5bd70d451394e79001fa9e8d06abe
JDCowBoy <280128206@qq.com>
2020-04-22 23:56:32 +0800

    增加footer版权,版本显示

--- src/main/java/com/kaoho/elf/controller/IndexController.java
+++ src/main/java/com/kaoho/elf/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.kaoho.elf.controller;
 
+import com.kaoho.elf.common.config.ElfBackendConfig;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
 
 /**
  * 首页
@@ -12,12 +16,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController extends BaseController
 {
 
+    @Resource
+    private ElfBackendConfig elfBackendConfig;
 
     //系统首页
     @GetMapping(value = "/")
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index()
+    public String index(ModelMap modelMap)
     {
+        modelMap.put("author", elfBackendConfig.getAuthor());
+        modelMap.put("version", elfBackendConfig.getVersion());
         return "index";
     }
 
